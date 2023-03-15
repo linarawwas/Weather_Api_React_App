a881885038ee9e23e2816965a8199a070d8d15bf
linarawwas <linamkraw2@gmail.com>
2023-03-15 15:20:21 +0100

    got rid of some files

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import mostlycloudy from '.././weather-icons/mostlycloudy.svg';
-import '../App.css'
- function Body (){
-    return(  
+import '../App.css';
+
+function Body (){
+return(  
         <div className="body">      
-        <img src={mostlycloudy} alt="dkash" />
+        <img src={mostlycloudy} alt="" />
         <h3>overcast clouds</h3>
-        <div className='one'><p className='strong'>Tempreture</p><p>10C to 11C</p> </div>
-        <div className='two'><p className='strong' >humidity</p> <p>78%</p> <p className='strong'>pressure</p><p>100.48</p>
+        <div className='one'><p className='strong'>Tempreture</p><p>10C to 11C</p></div>
+        <div className='two'><p className='strong'>humidity</p><p>78%</p><p className='strong'>pressure</p><p>100.48</p>
         </div>
         </div>
-)
+    )
 }
-export default Body
+export default Body;
